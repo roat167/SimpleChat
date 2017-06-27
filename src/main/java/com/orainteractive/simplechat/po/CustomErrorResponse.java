be7ea62edd0595e9e3550c1f6705ccdde510368f
khemroat <khemroat@gmail.com>
2017-06-27 13:05:47 -0700

    clean up code

--- src/main/java/com/orainteractive/simplechat/po/CustomErrorResponse.java
+++ src/main/java/com/orainteractive/simplechat/po/CustomErrorResponse.java
@@ -1,8 +1,6 @@
 package com.orainteractive.simplechat.po;
 
-import java.util.ArrayList;
 import java.util.HashMap;
-import java.util.List;
 
 public class CustomErrorResponse {
 	private String message;
