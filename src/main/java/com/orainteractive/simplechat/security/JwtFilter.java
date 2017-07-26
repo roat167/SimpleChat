6fb0df351e7e9dd8abda7b37b73460731045a691
khemroat <khemroat@gmail.com>
2017-07-26 16:13:47 -0700

    update filter to OncePerRequestFilter

--- src/main/java/com/orainteractive/simplechat/security/JwtFilter.java
+++ src/main/java/com/orainteractive/simplechat/security/JwtFilter.java
@@ -4,41 +4,39 @@ import java.io.IOException;
 
 import javax.servlet.FilterChain;
 import javax.servlet.ServletException;
-import javax.servlet.ServletRequest;
-import javax.servlet.ServletResponse;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.web.filter.GenericFilterBean;
+import org.springframework.web.filter.OncePerRequestFilter;
 
 import com.orainteractive.simplechat.constant.JwtConstant;
 import com.orainteractive.simplechat.exception.AuthenticationException;
 
 import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.SignatureException;
-
-public class JwtFilter extends GenericFilterBean {
+/**
+ * Using OncePerRequestFilter prevents the chain from calling the filter multiple times per request
+ */
+public class JwtFilter extends OncePerRequestFilter {
 	private static final Logger logger = LoggerFactory.getLogger(JwtFilter.class);	
 	
 	@Override
-	public void doFilter(final ServletRequest req, final ServletResponse res, final FilterChain chain)
+	protected void doFilterInternal(final HttpServletRequest req, final HttpServletResponse res, final FilterChain chain)
 			throws IOException, ServletException {
-
-		final HttpServletRequest request = (HttpServletRequest) req;
-		final HttpServletResponse response = (HttpServletResponse) res;
-		final String authHeader = request.getHeader(JwtConstant.AUTH_HEADER);
+		
+		final String authHeader = req.getHeader(JwtConstant.AUTH_HEADER);
 
 		logger.debug("Header " + authHeader);
-		if ("OPTIONS".equals(request.getMethod())) { //This need only if you want to by pass request which header is 'options'
-			response.setStatus(HttpServletResponse.SC_OK);
+		if ("OPTIONS".equals(req.getMethod())) { //This need only if you want to by pass request which header is 'options'
+			res.setStatus(HttpServletResponse.SC_OK);
 
 			chain.doFilter(req, res);
 		} else {
 			
 			//Get token value from cookie
-			String token = CookieUtil.getValue(request, JwtConstant.TOKEN_COOKIE);
+			String token = CookieUtil.getValue(req, JwtConstant.TOKEN_COOKIE);
 			logger.debug("TokenCookie::: " + token);
 	        if (token == null) {
 	        	throw new AuthenticationException("Missing or invalid Authorization header");
@@ -49,13 +47,12 @@ public class JwtFilter extends GenericFilterBean {
 	        //}
 			// The part after Bearer length = 7
 			// Extract the token from the HTTP Authorization header
-			//final String token = authHeader.substring(7);
-			
+			//final String token = authHeader.substring(7);			
 
 			try {
 				// validate the token				
 				final Claims claims = TokenHelper.getClaimsFromToken(token);
-				request.setAttribute("claims", claims);
+				req.setAttribute("claims", claims);
 				logger.debug("claim " + claims);
 
 			} catch (final SignatureException e) {
@@ -65,5 +62,5 @@ public class JwtFilter extends GenericFilterBean {
 
 			chain.doFilter(req, res);
 		}
-	}
+	}	
 }
